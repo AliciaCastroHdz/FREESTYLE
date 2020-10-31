c03c1950fb4377739185780ed51c06377d30c09f
monsecastro <acastro@weecompany.net>
2020-10-31 16:46:48 -0600

    Initial Commit

---
+++ FREESTYLE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FREESTYLE
+//
+//  Created by Alicia Monserrath Castro Hernandez on 31/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
